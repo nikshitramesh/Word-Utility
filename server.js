ec4980cabc8fa968ec6767f6101a46cb29ba8784
nikshitramesh <nikshitramesh@gmail.com>
2025-04-06 21:44:46 +0530

    Added first 4 files

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const path = require("path");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const app = express();
+app.use(express.json(), cors());
+app.use(express.static(path.join(__dirname, "build")));
+mongoose.connect("mongodb://localhost:27017/textutildb", {
+}).then(() => {
+    console.log("Connected to MongoDB successfully");
+}).catch((err) => {
+    console.log(err);
+});
+const schema = new mongoose.Schema({
+    name: String,
+    email: String,
+    dob: String,
+});
+const user = mongoose.model("users", schema);
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+app.post("/", (req, res) => {
+    const User = new user({
+        name: req.body.name,
+        email: req.body.email,
+        dob: req.body.dob,
+    });
+    User.save().then(() => {
+        console.log("Data saved successfully");
+        res.send("Logged in Successfully");
+        res.end();
+    }).catch((err) => {
+        console.log(err);
+    });
+});
+app.listen(5000, () => {
+    console.log("Server is running on http://localhost:5000/");
+});
