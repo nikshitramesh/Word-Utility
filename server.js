8da2df83e706bbc422840b24092da2aad5a52be1
nikshitramesh <nikshitramesh@gmail.com>
2025-04-07 21:58:41 +0530

    Updated server.js
    Comments from now on are saved in the database

--- server.js
+++ server.js
@@ -11,16 +11,20 @@ mongoose.connect("mongodb://localhost:27017/textutildb", {
 }).catch((err) => {
     console.log(err);
 });
-const schema = new mongoose.Schema({
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+const userschema = new mongoose.Schema({
     name: String,
     email: String,
     dob: String,
 });
-const user = mongoose.model("users", schema);
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "build", "index.html"));
+const cmtschema = new mongoose.Schema({
+    email: String,
+    comment: String
 });
-app.post("/", (req, res) => {
+const user = mongoose.model("users", userschema);
+app.post("/auth", (req, res) => {
     const User = new user({
         name: req.body.name,
         email: req.body.email,
@@ -28,7 +32,21 @@ app.post("/", (req, res) => {
     });
     User.save().then(() => {
         console.log("Data saved successfully");
-        res.send("Logged in Successfully");
+        res.send("Data saved successfully");
+        res.end();
+    }).catch((err) => {
+        console.log(err);
+    });
+});
+const review = mongoose.model("feedback", cmtschema);
+app.post("/reviews", (req, res) => {
+    const comment = new review({
+        email: req.body.email,
+        comment: req.body.comment,
+    });
+    comment.save().then(() => {
+        console.log("Comment saved successfully");
+        res.send("Comment uploaded successfully");
         res.end();
     }).catch((err) => {
         console.log(err);
