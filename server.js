665f80cc2812ca7de2c3c008aad91cc07f738630
nikshitramesh <nikshitramesh@gmail.com>
2025-04-08 21:46:16 +0530

    Updated server.js
    Added fetching and displaying the comments to the frontend

--- server.js
+++ server.js
@@ -11,9 +11,6 @@ mongoose.connect("mongodb://localhost:27017/textutildb", {
 }).catch((err) => {
     console.log(err);
 });
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "build", "index.html"));
-});
 const userschema = new mongoose.Schema({
     name: String,
     email: String,
@@ -39,18 +36,25 @@ app.post("/auth", (req, res) => {
     });
 });
 const review = mongoose.model("feedback", cmtschema);
-app.post("/reviews", (req, res) => {
+app.post("/reviews", async (req, res) => {
     const comment = new review({
         email: req.body.email,
         comment: req.body.comment,
     });
-    comment.save().then(() => {
+    await comment.save().then(() => {
         console.log("Comment saved successfully");
         res.send("Comment uploaded successfully");
         res.end();
     }).catch((err) => {
         console.log(err);
     });
+});
+app.get("/about", async (req, res) => {
+    const list = await review.find({});
+    res.json(list)
+});
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 app.listen(5000, () => {
     console.log("Server is running on http://localhost:5000/");
