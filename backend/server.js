ab53392c9e6feab2223b3c832ec84cfb0282e5d3
Hrutav Modha <modhahrutav@gmail.com>
2025-04-18 13:25:01 +0530

    Updated backend code

--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,13 @@ const express = require("express");
 const path = require("path");
 const cors = require("cors");
 const mongoose = require("mongoose");
+require("dotenv").config();
 const app = express();
 app.use(express.json(), cors());
 app.use(express.static(path.join(__dirname, "..", "frontend", "build")));
-mongoose.connect("mongodb://localhost:27017/textutildb", {
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
 }).then(() => {
     console.log("Connected to MongoDB successfully");
 }).catch((err) => {
