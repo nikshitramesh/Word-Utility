2ed5c0991c427258d4284fd1ee9dc5945674ac59
Hrutav Modha <modhahrutav@gmail.com>
2025-04-18 13:50:43 +0530

    Updated server.js

--- backend/server.js
+++ backend/server.js
@@ -58,6 +58,6 @@ app.get("/reviews", async (req, res) => {
 app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "build", "index.html"));
 });
-app.listen(`${port}`, () => {
+app.listen(port, () => {
     console.log("Server is running on render");
 });
